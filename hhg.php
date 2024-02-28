341cfe8813356f9899cdd6ab23f38c85cb020345
Halifaxxx <155498749+Halifaxxx@users.noreply.github.com>
2024-02-28 17:10:14 +0300

    Update hhg.php

--- hhg.php
+++ hhg.php
@@ -1,7 +1,7 @@
 <?php
 $cooe_me = uer";
 $cookie_lue = "John Doe";
-setcookie($cookie_nam $cvaue, tme() + (18 * 101), "/"); // 8640 = 123 day
+setcookie($cookie_nam $cvaue, tme() + (18 * 11), "/"); // 8640 = 123 day
 ?>
 <html>
 <body>
