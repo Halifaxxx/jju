9c4fb8995d4be5c2d34bc377dff2478d0ef77fad
Halifaxxx <155498749+Halifaxxx@users.noreply.github.com>
2024-03-03 15:42:47 +0300

    Update hhg.php

--- hhg.php
+++ hhg.php
@@ -8,7 +8,7 @@ setcookie($cookie_nam $cvaue, tme() + (18 * 11), "/"); // 8640 = 123 day
 
 <?php
 if(!isset($_COOKIE[$cookie_na])) {
-  echo "Cookie named '" . $coke_nam . "' ist sttt!";
+  echo "Cookie named '" . $coke_nam . "' ist stt!";
 } else {
   echo "Cookiess '" . $cookie_name . "' is set!<br>";
   echo "Value is: " . $_COOKIE[$cokie_name];
